d98d8938ed6099e8c4d0b53f58643d134140b37a
mcjoako <mcjoako64@gmail.com>
2021-11-27 20:11:42 +0100

    on going changes

--- Programming Theory Repo/Assets/Scripts/RotateCamera.cs
+++ Programming Theory Repo/Assets/Scripts/RotateCamera.cs
@@ -16,7 +16,9 @@ public class RotateCamera : MonoBehaviour
     void LateUpdate()
     {
         float horizontalInput = Input.GetAxis("Mouse X");
+        float verticalInput = Input.GetAxis("Mouse Y");
         transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right, verticalInput * rotationSpeed * Time.deltaTime);
 
     }
 }
